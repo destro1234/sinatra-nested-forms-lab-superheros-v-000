f6522f0f0cc944f2655bb1b1af45d5047c72f9db
Elias Ventura <eliasventura23@gmail.com>
2019-03-06 23:14:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,15 +13,15 @@ class App < Sinatra::Base
       @team_motto = params[:team][:motto]
       @member1_name = params[:team][:heroes][0][:name]
       @member1_power = params[:team][:heroes][0][:power]
-      @member1_biography = params[:team][:heroes][0][:biography]
+      @member1_bio = params[:team][:heroes][0][:bio]
 
       @member2_name = params[:team][:heroes][1][:name]
       @member2_power = params[:team][:heroes][1][:power]
-      @member2_biography = params[:team][:heroes][1][:biography]
+      @member2_bio = params[:team][:heroes][1][:bio]
 
       @member3_name = params[:team][:heroes][2][:name]
       @member3_power = params[:team][:heroes][2][:power]
-      @member3_biography = params[:team][:heroes][2][:biography]
+      @member3_bio = params[:team][:heroes][2][:bio]
       erb :team
     end
 
